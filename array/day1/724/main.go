7454237f734bfbde6d83d82a1aca2495509cfff3
yuchao <alex.yu@danielwellington.com>
2020-07-01 18:22:44 +0800

    leetcode day1 revision

--- array/day1/724/main.go
+++ array/day1/724/main.go
@@ -1,9 +1,13 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
+/**
+#724
+给定一个整数类型的数组 nums，请编写一个能够返回数组 “中心索引” 的方法。
+我们是这样定义数组 中心索引 的：数组中心索引的左侧所有元素相加的和等于右侧所有元素相加的和。
+如果数组不存在中心索引，那么我们应该返回 -1。如果数组有多个中心索引，那么我们应该返回最靠近左边的那一个。
+*/
 func main() {
 	var nums = []int{1, 7, 3, 6, 5, 6}
 	var nums1 = []int{1, 2, 3}
@@ -19,20 +23,17 @@ func main() {
 	fmt.Println(pivotIndex(nums5))
 }
 
-// 寻找数组的中心索引
 func pivotIndex(nums []int) int {
 	var total int
 	for _, v := range nums {
 		total += v
 	}
-	for i := 0; i < len(nums); i++ {
-		var left int
-		for j := 0; j < i; j++ {
-			left += nums[j]
+	var left int
+	for k, v := range nums {
+		if left*2+v == total {
+			return k
 		}
-		if left*2+nums[i] == total {
-			return i
-		}
+		left += v
 	}
 	return -1
 }
