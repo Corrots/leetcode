b7646415424e8f3ba0a25c9657d505fd37626aba
yuchao <alex.yu@danielwellington.com>
2020-07-07 11:40:11 +0800

    leetcode #48

--- array/day5/48/main.go
+++ array/day5/48/main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 /**
 #01.07
+#48
 旋转矩阵
 给你一幅由 N × N 矩阵表示的图像，其中每个像素的大小为 4 字节。请你设计一种算法，将图像旋转 90 度。
 不占用额外内存空间能否做到？
@@ -17,15 +18,27 @@ func main() {
 	fmt.Println(matrix2)
 }
 
+/**
+1 2 3
+4 5 6
+7 8 9
+// 以4 5 6为中线翻转
+7 8 9
+4 5 6
+1 2 3
+// 以对角线753为中线翻转
+
+*/
+
 func rotate(matrix [][]int) {
 	n := len(matrix)
-	// 每一行水平翻转
+	// 以水平线456为中线翻转
 	for i := 0; i < n/2; i++ {
 		for j := 0; j < n; j++ {
-			matrix[i][j], matrix[n-1-i][j] = matrix[n-1-i][j], matrix[i][j]
+			matrix[i][j], matrix[n-i-1][j] = matrix[n-i-1][j], matrix[i][j]
 		}
 	}
-	// 对角线翻转
+	// 以对角线753为中线翻转
 	for i := 0; i < n; i++ {
 		for j := 0; j < i; j++ {
 			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
