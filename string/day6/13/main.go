1e67605274bb6f7cde285bf8c34d614503a72fc7
yuchao <alex.yu@danielwellington.com>
2020-07-08 10:26:48 +0800

    leetcode #13

---
+++ string/day6/13/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	str1 := "III"
+	str2 := "IV"
+	str3 := "IX"
+	str4 := "LVIII"
+	str5 := "MCMXCIV"
+	fmt.Println(romanToInt(str1))
+	fmt.Println(romanToInt(str2))
+	fmt.Println(romanToInt(str3))
+	fmt.Println(romanToInt(str4))
+	fmt.Println(romanToInt(str5))
+}
+
+func romanToInt(s string) int {
+
+}
