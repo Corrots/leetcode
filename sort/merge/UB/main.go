ee2f8996775f77fb7a137e2191050e98171b922b
yuchao <alex.yu@danielwellington.com>
2020-07-15 12:09:01 +0800

    modify merge sort

--- sort/merge/UB/main.go
+++ sort/merge/UB/main.go
@@ -7,13 +7,14 @@ import (
 	"github.com/corrots/leetcode/sort/helper"
 )
 
+// 自顶向下
 func main() {
 	nums1 := []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
 	MergeSort(nums1)
 	fmt.Println(nums1)
 	nums2 := helper.GenerateRandomData(100000, 0, 1000000)
-	nums3 := make([]int, 100000)
-	copy(nums3, nums2)
+	//nums3 := make([]int, 100000)
+	//copy(nums3, nums2)
 	MergeSort(nums2)
 }
 
@@ -28,7 +29,6 @@ func InsertionSort(nums []int, l, r int) {
 	}
 }
 
-// 自顶向下
 func MergeSort(nums []int) {
 	n := len(nums)
 	start := time.Now()
@@ -38,6 +38,9 @@ func MergeSort(nums []int) {
 
 // 递归使用归并排序对nums[l:r]范围的元素进行排序
 func mergeSort(nums []int, l, r int) {
+	//if l >= r {
+	//	return
+	//}
 	if r-l <= 15 {
 		InsertionSort(nums, l, r)
 		return
@@ -45,19 +48,21 @@ func mergeSort(nums []int, l, r int) {
 	mid := (l + r) / 2
 	mergeSort(nums, l, mid)
 	mergeSort(nums, mid+1, r)
-	if nums[mid] > nums[mid+1] {
+	if nums[mid] <= nums[mid+1] {
 		merge(nums, l, mid, r)
 	}
 }
 
 // 将nums[l:mid]和nums[mid+1:r]进行归并
 func merge(nums []int, l, mid, r int) {
-	aux := make([]int, r-l+1)
+	aux := make([]int, (r-l)+1)
 	for i := l; i <= r; i++ {
 		aux[i-l] = nums[i]
 	}
+	// i, j分别位于左右数组的起始位置
 	i, j := l, mid+1
 	for k := l; k <= r; k++ {
+		// 避免i/j越界
 		if i > mid {
 			nums[k] = aux[j-l]
 			j++
@@ -72,4 +77,5 @@ func merge(nums []int, l, mid, r int) {
 			j++
 		}
 	}
+
 }
