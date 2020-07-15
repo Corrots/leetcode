a186f1799dccfde14ae0e6498985beae6377f8df
yuchao <alex.yu@danielwellington.com>
2020-07-15 18:02:08 +0800

    quick sort v2 & v3

--- sort/helper/tools.go
+++ sort/helper/tools.go
@@ -17,3 +17,14 @@ func GenerateRandomData(n, rangeL, rangeR int) []int {
 	}
 	return res
 }
+
+func InsertionSort(nums []int, l, r int) {
+	for i := l + 1; i <= r; i++ {
+		e := nums[i]
+		j := 0
+		for j = i; j > l && nums[j-1] > e; j-- {
+			nums[j] = nums[j-1]
+		}
+		nums[j] = e
+	}
+}
--- sort/quick/v3/main.go
+++ sort/quick/v3/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 
 	"github.com/corrots/leetcode/sort/helper"
 )
 
+// 优化对于大量重复元素的排序
+// 双路快排
 func main() {
 	nums1 := []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
 	QuickSort(nums1)
@@ -15,16 +18,22 @@ func main() {
 	nums3 := make([]int, 1000000)
 	copy(nums3, nums2)
 	QuickSort(nums2)
+	nums4 := helper.GenerateRandomData(1000000, 0, 10)
+	QuickSort(nums4)
 }
-
 func QuickSort(nums []int) {
 	start := time.Now()
+	rand.Seed(time.Now().UnixNano())
 	quickSort(nums, 0, len(nums)-1)
-	fmt.Printf("spent: %d ms\n", time.Since(start).Milliseconds())
+	fmt.Printf("quick: %d ms\n", time.Since(start).Milliseconds())
 }
 
 func quickSort(nums []int, l, r int) {
-	if l >= r {
+	//if l >= r {
+	//	return
+	//}
+	if r-l <= 15 {
+		helper.InsertionSort(nums, l, r)
 		return
 	}
 	p := partition(nums, l, r)
@@ -34,17 +43,25 @@ func quickSort(nums []int, l, r int) {
 
 // 返回值p使 nums[l:p-1] < nums[p] < nums[p+1: r]
 func partition(nums []int, l, r int) (p int) {
+	random := rand.Int()%(r-l+1) + l
+	nums[l], nums[random] = nums[random], nums[l]
 	v := nums[l]
-	j := l
-	// 通过初始化j=l，使得循环中nums[l+1:j-1]和nums[j+1:i)都初始化为空
-	for i := l + 1; i <= r; i++ {
-		if nums[i] < v {
-			nums[i], nums[j+1] = nums[j+1], nums[i]
-			j++
+	// nums[l+1,i) <= v; nums(j:r] >= v
+	i, j := l+1, r
+	for {
+		for i <= r && nums[i] < v {
+			i++
 		}
+		for j >= l+1 && nums[j] > v {
+			j--
+		}
+		if i > j {
+			break
+		}
+		nums[i], nums[j] = nums[j], nums[i]
+		i++
+		j--
 	}
-	// 此时 nums[l+1:j]<v; nums[j+1:i) > v
-	// 把l和j位置的元素互换位置，整个[l:r]就完成有序了
 	nums[l], nums[j] = nums[j], nums[l]
 	return j
 }
