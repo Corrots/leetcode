d510db0c9e8f196a4673efc1322ed28c3ee2b9b5
yuchao <alex.yu@danielwellington.com>
2020-07-15 15:55:45 +0800

    quick sort

--- sort/merge/BU/main.go
+++ sort/merge/BU/main.go
@@ -10,23 +10,22 @@ import (
 // 自底向上
 func main() {
 	nums1 := []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
-	MergeSort(nums1)
+	MergeSortBU(nums1)
 	fmt.Println(nums1)
 	nums2 := helper.GenerateRandomData(100000, 0, 1000000)
-	nums3 := make([]int, 100000)
-	copy(nums3, nums2)
-	MergeSort(nums2)
+	MergeSortBU(nums2)
 }
 
-func MergeSort(nums []int) {
+func MergeSortBU(nums []int) {
 	n := len(nums)
 	start := time.Now()
-	for sz := 1; sz <= n; sz += sz {
+	for sz := 1; sz < n; sz += sz {
 		for i := 0; i+sz < n; i += sz + sz {
-			edge := n - 1
-			if i+sz+sz-1 < edge {
-				edge = i + sz + sz - 1
+			edge := i + sz + sz - 1
+			if edge > n-1 {
+				edge = n - 1
 			}
+			// 对nums[i:i+sz-1]和nums[i+sz:i+2*sz-1]进行归并
 			merge(nums, i, i+sz-1, edge)
 		}
 	}
