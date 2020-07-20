a881d9a813feaff631153644d838fe4d7fdc602e
alex yu <alex.yu@danielwellington.com>
2020-07-20 22:57:40 +0800

    BFS & maximum and minimum in binary search tree

---
+++ search/binarysearch/v2/bst/node.go
@@ -0,0 +1,110 @@
+package bst
+
+import "fmt"
+
+type Node struct {
+	Key   int
+	Value interface{}
+	left  *Node
+	right *Node
+}
+
+func newNode(key int, value interface{}) *Node {
+	return &Node{
+		Key:   key,
+		Value: value,
+	}
+}
+
+// 在以node为根的二叉搜索树中，返回最小键值的节点
+func (n *Node) minimum() *Node {
+	if n.left == nil {
+		return n
+	}
+	return n.left.minimum()
+}
+
+func (n *Node) maximum() *Node {
+	if n.right == nil {
+		return n
+	}
+	return n.right.maximum()
+}
+
+func (n *Node) insert(key int, value interface{}) *Node {
+	if n == nil {
+		return newNode(key, value)
+	}
+
+	if key == n.Key {
+		n.Value = value
+	} else if key < n.Key {
+		n.left = n.left.insert(key, value)
+	} else {
+		n.right = n.right.insert(key, value)
+	}
+	return n
+}
+
+func (n *Node) contain(key int) bool {
+	if n == nil {
+		return false
+	}
+
+	if key == n.Key {
+		return true
+	} else if key < n.Key {
+		return n.left.contain(key)
+	} else {
+		return n.right.contain(key)
+	}
+}
+
+func (n *Node) search(key int) interface{} {
+	if n == nil {
+		return nil
+	}
+	if key == n.Key {
+		return n.Value
+	} else if key < n.Key {
+		return n.left.search(key)
+	} else {
+		return n.right.search(key)
+	}
+}
+
+// 对Node进行前序遍历
+func (n *Node) preOrder() {
+	if n != nil {
+		fmt.Printf("%d ", n.Key)
+		n.left.preOrder()
+		n.right.preOrder()
+	}
+}
+
+// 对Node进行中序遍历
+func (n *Node) inOrder() {
+	if n != nil {
+		n.left.inOrder()
+		fmt.Printf("%d ", n.Key)
+		n.right.inOrder()
+	}
+}
+
+// 对Node进行后序遍历
+func (n *Node) postOrder() {
+	if n != nil {
+		n.left.postOrder()
+		n.right.postOrder()
+		fmt.Printf("%d ", n.Key)
+	}
+}
+
+// 析构函数，对二叉搜索树进行空间释放
+func (n *Node) destroy() {
+	if n != nil {
+		n.left.destroy()
+		n.right.destroy()
+		n.destroy()
+	}
+}
--- search/binarysearch/v2/bst/tree.go
+++ search/binarysearch/v2/bst/tree.go
@@ -7,11 +7,20 @@ type BST struct {
 	count int
 }
 
-type Node struct {
-	Key   int
-	Value interface{}
-	left  *Node
-	right *Node
+func NewBST() *BST {
+	return &BST{
+		root:  nil,
+		count: 0,
+	}
+}
+
+// 返回二叉搜索树中的最小键值
+func (b *BST) Minimum() int {
+	return b.root.minimum().Key
+}
+
+func (b *BST) Maximum() int {
+	return b.root.maximum().Key
 }
 
 // 前序遍历
@@ -19,25 +28,9 @@ func (b *BST) PreOrder() {
 	b.root.preOrder()
 }
 
-func (n *Node) preOrder() {
-	if n != nil {
-		fmt.Printf("%d ", n.Key)
-		n.left.preOrder()
-		n.right.preOrder()
-	}
-}
-
 // 中序遍历
 func (b *BST) InOrder() {
 	b.root.inOrder()
-}
-
-func (n *Node) inOrder() {
-	if n != nil {
-		n.left.inOrder()
-		fmt.Printf("%d ", n.Key)
-		n.right.inOrder()
-	}
 }
 
 // 后序遍历
@@ -45,11 +38,20 @@ func (b *BST) PostOrder() {
 	b.root.postOrder()
 }
 
-func (n *Node) postOrder() {
-	if n != nil {
-		n.left.postOrder()
-		n.right.postOrder()
-		fmt.Printf("%d ", n.Key)
+// 层序遍历(广度优先遍历)
+func (b *BST) levelOrder() {
+	var q []*Node
+	q = append(q, b.root)
+	for len(q) > 0 {
+		node := q[0]
+		q = q[1:]
+		fmt.Printf("%d ", node.Key)
+		if node.left != nil {
+			q = append(q, node.left)
+		}
+		if node.right != nil {
+			q = append(q, node.right)
+		}
 	}
 }
 
@@ -57,61 +59,12 @@ func (b *BST) Search(key int) interface{} {
 	return b.root.search(key)
 }
 
-func (n *Node) search(key int) interface{} {
-	if n == nil {
-		return nil
-	}
-	if key == n.Key {
-		return n.Value
-	} else if key < n.Key {
-		return n.left.search(key)
-	} else {
-		return n.right.search(key)
-	}
-}
-
 func (b *BST) Contain(key int) bool {
 	return b.root.contain(key)
 }
 
-func (n *Node) contain(key int) bool {
-	if n == nil {
-		return false
-	}
-
-	if key == n.Key {
-		return true
-	} else if key < n.Key {
-		return n.left.contain(key)
-	} else {
-		return n.right.contain(key)
-	}
-}
-
 func (b *BST) Insert(key int, value interface{}) {
 	b.root = b.root.insert(key, value)
-}
-
-func (n *Node) insert(key int, value interface{}) *Node {
-	if n == nil {
-		return newNode(key, value)
-	}
-
-	if key == n.Key {
-		n.Value = value
-	} else if key < n.Key {
-		n.left = n.left.insert(key, value)
-	} else {
-		n.right = n.right.insert(key, value)
-	}
-	return n
-}
-
-func NewBST() *BST {
-	return &BST{
-		root:  nil,
-		count: 0,
-	}
 }
 
 func (b *BST) Len() int {
@@ -122,9 +75,7 @@ func (b *BST) IsEmpty() bool {
 	return b.count == 0
 }
 
-func newNode(key int, value interface{}) *Node {
-	return &Node{
-		Key:   key,
-		Value: value,
-	}
+func (b *BST) Destroy() {
+	b.root.destroy()
+	b.count--
 }
