31db09b522ff6f2f3455af2eb420ec15cf883974
yuchao <alex.yu@danielwellington.com>
2020-07-23 14:07:25 +0800

    union find 路径压缩Path Compression

--- unionfind/test/main.go
+++ unionfind/test/main.go
@@ -5,6 +5,8 @@ import (
 	"math/rand"
 	"time"
 
+	"github.com/corrots/leetcode/unionfind/uf5"
+
 	"github.com/corrots/leetcode/unionfind/uf2"
 	"github.com/corrots/leetcode/unionfind/uf3"
 	"github.com/corrots/leetcode/unionfind/uf4"
@@ -12,8 +14,9 @@ import (
 
 func main() {
 	n := 1000000
-	TestUF3(n)
+	//TestUF3(n)
 	TestUF4(n)
+	TestUF5(n)
 }
 
 func TestUF2(n int) {
@@ -68,4 +71,22 @@ func TestUF4(n int) {
 		uf.IsConnected(a, b)
 	}
 	fmt.Printf("UF4, %d ops, %d ms\n", 2*n, time.Since(start).Milliseconds())
+}
+
+func TestUF5(n int) {
+	rand.Seed(time.Now().UnixNano())
+	uf := uf5.New(n)
+	start := time.Now()
+
+	for i := 0; i < n; i++ {
+		a := rand.Int() % n
+		b := rand.Int() % n
+		uf.Union(a, b)
+	}
+	for i := 0; i < n; i++ {
+		a := rand.Int() % n
+		b := rand.Int() % n
+		uf.IsConnected(a, b)
+	}
+	fmt.Printf("UF5, %d ops, %d ms\n", 2*n, time.Since(start).Milliseconds())
 }
