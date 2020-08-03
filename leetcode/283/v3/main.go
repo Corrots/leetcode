ad08d30d656ae4dcb9b5748ee14e7f32ece8987b
alex yu <alex.yu@danielwellington.com>
2020-08-03 22:05:01 +0800

    leetcode #283 v3

--- leetcode/283/v3/main.go
+++ leetcode/283/v3/main.go
@@ -15,10 +15,11 @@ func moveZeroes(nums []int) {
 	k := 0
 	for i := 0; i < len(nums); i++ {
 		if nums[i] != 0 {
-			if i != k { //
-				nums[k], nums[i] = nums[i], nums[k]
-			}
+			nums[k] = nums[i]
 			k++
 		}
+	}
+	for i := k; i < len(nums); i++ {
+		nums[i] = 0
 	}
 }
