c35f2eb2323392bc780f55ca0433c4a0e1eee0e0
alex yu <alex.yu@danielwellington.com>
2020-08-03 21:58:53 +0800

    leetcode #283

---
+++ leetcode/283/v2/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{0, 1, 0, 3, 12}
+	moveZeroes(nums)
+	fmt.Println(nums)
+}
+
+// 时间复杂度: O(n)
+// 空间复杂度: O(1)
+func moveZeroes(nums []int) {
+	// nums[0:k)中的元素都是非0元素
+	k := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			nums[k] = nums[i]
+			k++
+		}
+	}
+	for i := k; i < len(nums); i++ {
+		nums[i] = 0
+	}
+}
