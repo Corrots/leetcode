8528fc89c6cbb11f29eb694b3abc7eebb0425db3
alex.yu <alex.yu@danielwellington.com>
2020-08-06 13:23:35 +0800

    leetcode #209

---
+++ leetcode/209/v1/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	nums := []int{2, 3, 1, 2, 4, 3}
+	fmt.Println(minSubArrayLen(7, nums))
+}
+
+// https://leetcode-cn.com/problems/minimum-size-subarray-sum/
+// 暴力解法
+func minSubArrayLen(s int, nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+	res := math.MaxInt64
+	for i := 0; i < n; i++ {
+		sum := 0
+		for j := i; j < n; j++ {
+			sum += nums[j]
+			if sum >= s {
+				res = min(res, j-i+1)
+				break
+			}
+		}
+	}
+	if res == math.MaxInt64 {
+		return 0
+	}
+	return res
+}
+
+func min(i, j int) int {
+	if i < j {
+		return i
+	}
+	return j
+}
