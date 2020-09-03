eabd8dad3ac07535757fbf2a4e0f2d0df7a131cf
alex.yu <alex.yu@danielwellington.com>
2020-09-03 15:11:48 +0800

    leetcode #447

--- leetcode/447/main.go
+++ leetcode/447/main.go
@@ -1,27 +1,32 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	nums := [][]int{{0, 0}, {1, 0}, {2, 0}}
+	fmt.Println(numberOfBoomerangs(nums))
 }
 
 // https://leetcode-cn.com/problems/number-of-boomerangs/
 func numberOfBoomerangs(points [][]int) int {
-	var num int
+	var res int
 	for i := range points {
-		ht := make(map[int]int, len(points))
+		record := make(map[int]int)
 		for j := range points {
-			if i != j {
-				dist := dist(points[i], points[j])
-				if cnt, ok := ht[dist]; ok {
-					num += cnt * 2
-				}
-				ht[dist]++
+			if j != i {
+				//fmt.Printf("point i: %v, point j: %v\n", points[i], points[j])
+				record[distance(points[i], points[j])]++
+			}
+		}
+		for _, v := range record {
+			if v >= 2 {
+				res += v * (v - 1)
 			}
 		}
 	}
-	return num
+	return res
 }
 
-func dist(i []int, j []int) int {
+func distance(i []int, j []int) int {
 	return (i[0]-j[0])*(i[0]-j[0]) + (i[1]-j[1])*(i[1]-j[1])
 }
