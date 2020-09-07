58aa2307b601d16ef1b3bbfd86da5d9e53e7015c
alex.yu <alex.yu@danielwellington.com>
2020-09-07 16:43:42 +0800

    leetcode #437 modify

--- bst/leetcode/437/main.go
+++ bst/leetcode/437/main.go
@@ -5,6 +5,7 @@ func main() {
 }
 
 //https://leetcode-cn.com/problems/path-sum-iii/
+// 在以root为根节点的二叉树中寻找和为sum的路径个数
 func pathSum(root *TreeNode, sum int) int {
 	if root == nil {
 		return 0
@@ -13,15 +14,16 @@ func pathSum(root *TreeNode, sum int) int {
 	return res + pathSum(root.Left, sum) + pathSum(root.Right, sum)
 }
 
-func findPath(root *TreeNode, sum int) int {
-	if root == nil {
+// 在以node为根节点的二叉树中，寻找从node开始的路径，且路径的和为sum的个数
+func findPath(node *TreeNode, sum int) int {
+	if node == nil {
 		return 0
 	}
 	var res int
-	if sum == root.Val {
+	if sum == node.Val {
 		res += 1
 	}
-	return res + findPath(root.Left, sum-root.Val) + findPath(root.Right, sum-root.Val)
+	return res + findPath(node.Left, sum-node.Val) + findPath(node.Right, sum-node.Val)
 }
 
 //
