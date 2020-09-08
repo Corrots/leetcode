48d7f522b4ae484702c3349474817055a3d17d40
alex yu <alex.yu@danielwellington.com>
2020-09-08 22:58:26 +0800

    leetcode #17

--- tree/17/main.go
+++ tree/17/main.go
@@ -4,6 +4,42 @@ func main() {
 
 }
 
-func letterCombinations(digits string) []string {
+/**
+digits 代表数字字符串
+s(digits)表示digits所能代表的字母字符串
+s(digits[0...n-1])
+	= letter(digits[0]) + s(digits[1...n-1])
+	= letter(digits[0]) + letter(digits[1]) + s(digits[2...n-1])
+*/
 
+var letterMap = [10]string{
+	" ",
+	"", "abc", "def",
+	"ghi", "jki", "mno",
+	"pqrs", "tuv", "wxyz",
+}
+
+//https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/
+func letterCombinations(digits string) []string {
+	res = []string{}
+	if len(digits) == 0 {
+		return res
+	}
+	findCombinations(&digits, 0, "")
+	return res
+}
+
+var res []string
+
+func findCombinations(digits *string, index int, s string) {
+	if index == len(*digits) {
+		// 保存s
+		res = append(res, s)
+		return
+	}
+	char := (*digits)[index]
+	letters := letterMap[char-'0']
+	for i := 0; i < len(letters); i++ {
+		findCombinations(digits, index+1, s+string(letters[i]))
+	}
 }
