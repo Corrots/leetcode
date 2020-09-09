51e99615927a7cca48c1693cdd74cd3b3880796a
alex.yu <alex.yu@danielwellington.com>
2020-09-09 16:46:05 +0800

    leetcode #47 modify

--- tree/47/main.go
+++ tree/47/main.go
@@ -13,39 +13,39 @@ func main() {
 //https://leetcode-cn.com/problems/permutations-ii/
 func permuteUnique(nums []int) [][]int {
 	res = [][]int{}
-	used := make([]bool, len(nums))
+	used = make([]bool, len(nums))
 	if len(nums) == 0 {
 		return res
 	}
 	sort.Ints(nums)
-	getPermutation(nums, 0, []int{}, used)
+	getPermutation(nums, 0, []int{})
 	return res
 }
 
 var (
-	res [][]int
+	res  [][]int
+	used []bool
 )
 
-func getPermutation(nums []int, index int, s []int, used []bool) {
+func getPermutation(nums []int, index int, s []int) {
 	if index == len(nums) {
 		tmp := make([]int, len(nums))
 		copy(tmp, s)
 		res = append(res, tmp)
 	} else {
-		for k, v := range nums {
-			if used[k] {
+		for i := 0; i < len(nums); i++ {
+			if used[i] {
 				continue
 			}
 			// 剪枝
-			if k > 0 && v == nums[k-1] && !used[k-1] {
+			if i > 0 && nums[i] == nums[i-1] && !used[i-1] {
 				continue
 			}
-			s = append(s, v)
-			used[k] = true
-			getPermutation(nums, index+1, s, used)
+			s = append(s, nums[i])
+			used[i] = true
+			getPermutation(nums, index+1, s)
 			s = s[:len(s)-1]
-			used[k] = false
-
+			used[i] = false
 		}
 	}
 }
