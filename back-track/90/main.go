cace85492cfaf7915ce32bbfc9333deccb4b5359
alex yu <alex.yu@danielwellington.com>
2020-09-10 23:03:46 +0800

    leetcode #90

--- back-track/90/main.go
+++ back-track/90/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
 	//nums := []int{1, 2, 2}
 	nums := []int{4, 4, 4, 1, 4}
 	s := subsetsWithDup(nums)
-	fmt.Println(s, len(s))
+	fmt.Printf("%v\nlen: %d\n", s, len(s))
 }
 
 //https://leetcode-cn.com/problems/subsets-ii/
@@ -18,12 +19,12 @@ func subsetsWithDup(nums []int) [][]int {
 	if len(nums) == 0 {
 		return res
 	}
-	//sort.Ints(nums)
+	sort.Ints(nums)
 	var dfs func(int, []int)
 	dfs = func(start int, path []int) {
 		for i := start; i < len(nums); i++ {
-			for i > start && nums[i] == nums[i-1] {
-				i++
+			if i > start && nums[i] == nums[i-1] {
+				continue
 			}
 			tmp := make([]int, len(path))
 			copy(tmp, path)
