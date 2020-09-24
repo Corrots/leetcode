614fd57d63a715994e02a1eb037c0e650eac7cfc
alex yu <alex.yu@danielwellington.com>
2020-09-24 21:59:48 +0800

    leetcode #141

---
+++ one-question-per-day/141/main.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{3, 2, 0, -4}
+	head := CreateLinkedList(nums, len(nums))
+	PrintLinkedList(head)
+}
+
+//https://leetcode-cn.com/problems/linked-list-cycle/
+func hasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+	slow, fast := head, head.Next
+	for fast != nil && fast.Next != nil {
+		if fast == slow {
+			return true
+		}
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+	return false
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func NewListNode(val int) *ListNode {
+	return &ListNode{Val: val}
+}
+
+func CreateLinkedList(nums []int, n int) *ListNode {
+	if n == 0 {
+		return nil
+	}
+	head := NewListNode(nums[0])
+	cur := head
+	for i := 1; i < n; i++ {
+		cur.Next = NewListNode(nums[i])
+		cur = cur.Next
+	}
+	return head
+}
+
+func PrintLinkedList(head *ListNode) {
+	cur := head
+	for cur != nil {
+		fmt.Printf("%d->", cur.Val)
+		cur = cur.Next
+	}
+	fmt.Printf("nil\n")
+}
