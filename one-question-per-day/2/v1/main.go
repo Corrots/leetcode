2c51b474b29da802c7f69349fe5c49a7566754c4
alex.yu <alex.yu@danielwellington.com>
2020-10-04 22:46:06 +0800

    leetcode #2 v1

--- one-question-per-day/2/v1/main.go
+++ one-question-per-day/2/v1/main.go
@@ -3,13 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	nums1 := []int{2, 4, 3}
+	nums1 := []int{3, 2, 1}
 	l1 := CreateLinkedList(nums1, len(nums1))
-	nums2 := []int{5, 6, 4}
+	nums2 := []int{9}
 	l2 := CreateLinkedList(nums2, len(nums2))
 	PrintLinkedList(addTwoNumbers(l1, l2))
 }
 
+//	1.获取链表l1和l2的长度，将短的链表末尾补0
+//	2.依次取出链表中的每一位，来进行相加操作，注意进位
 //https://leetcode-cn.com/problems/add-two-numbers/
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	if l1 == nil {
@@ -18,35 +20,36 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	if l2 == nil {
 		return l1
 	}
-	dummy := &ListNode{}
-	p, q, cur := l1, l2, dummy
+	// 将l1和l2中较短链表缺省部分补0
+	p, q := l1, l2
+	for p.Next != nil || q.Next != nil {
+		if p.Next == nil {
+			p.Next = &ListNode{Val: 0}
+		}
+		if q.Next == nil {
+			q.Next = &ListNode{Val: 0}
+		}
+		p = p.Next
+		q = q.Next
+	}
+	//3->2->1->nil
+	//9->0->0->nil
+	// 每位求和，并进位
+	l3 := &ListNode{}
+	res := l3
 	var carry int
-	for p != nil || q != nil {
-		var x, y int
-		if p != nil {
-			x = p.Val
-		} else {
-			x = 0
-		}
-		if q != nil {
-			y = q.Val
-		} else {
-			y = 0
-		}
-		sum := carry + x + y
-		carry = sum / 10
-		cur.Next = &ListNode{Val: sum % 10}
-		if p != nil {
-			p = p.Next
-		}
-		if q != nil {
-			q = q.Next
-		}
+	for l1 != nil {
+		sum := l1.Val + l2.Val + carry
+		l3.Next = &ListNode{Val: sum % 10} // 2->3
+		carry = sum / 10                   // 1->0
+		l3 = l3.Next
+		l1 = l1.Next
+		l2 = l2.Next
 	}
 	if carry > 0 {
-		cur.Next = &ListNode{Val: carry}
+		l3.Next = &ListNode{Val: carry % 10}
 	}
-	return dummy.Next
+	return res.Next
 }
 
 //
