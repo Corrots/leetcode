d55d17b19f8f7c421b4a8c94476a759ef7520107
alex yu <alex.yu@danielwellington.com>
2020-10-08 23:29:41 +0800

    leetcode offer #03 v2

--- offer/03/v2/main.go
+++ offer/03/v2/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	nums := []int{2, 3, 1, 0, 2, 5, 3}
@@ -10,5 +13,12 @@ func main() {
 // TODO 尝试空间复杂度O(1)的解法
 //https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/
 func findRepeatNumber(nums []int) int {
-
+	// 先排序后，重复的元素必然相邻
+	sort.Ints(nums)
+	for i := 1; i < len(nums); i++ {
+		if nums[i-1] == nums[i] {
+			return nums[i-1]
+		}
+	}
+	return -1
 }
