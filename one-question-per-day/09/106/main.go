2e3962e7d84fe320789d8895df4e56dd5402203d
alex.yu <alex.yu@danielwellington.com>
2020-10-23 11:19:09 +0800

    leetcode #106 modify

--- one-question-per-day/09/106/main.go
+++ one-question-per-day/09/106/main.go
@@ -4,29 +4,29 @@ func main() {
 
 }
 
+// 1.后序遍历的最后一个节点即为根节点的val，通过val找到其在中序遍历的位置，左右两侧分别为左右子树的节点
+// 2.重新通过递归构建二叉树
+
 //https://leetcode-cn.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
 func buildTree(inorder []int, postorder []int) *TreeNode {
-	m := make(map[int]int)
-	for k, v := range inorder {
-		m[v] = k
+	iLen, pLen := len(inorder), len(postorder)
+	if iLen == 0 || pLen == 0 {
+		return nil
 	}
-	var build func(int, int) *TreeNode
-	build = func(left, right int) *TreeNode {
-		if left > right {
-			return nil
+	var rootIndex int
+	rootVal := postorder[pLen-1]
+	for i := 0; i < iLen; i++ {
+		if inorder[i] == rootVal {
+			rootIndex = i
+			break
 		}
-		// 后序遍历的最后一个元素即为根节点
-		n := len(postorder)
-		val := postorder[n-1]
-		postorder = postorder[:n-1]
-		// 获取根节点在inorder中的索引，其右侧即为右子树的元素，左侧为左子树的元素
-		rootIndex := m[val]
-		root := &TreeNode{Val: val}
-		root.Right = build(rootIndex+1, right)
-		root.Left = build(left, rootIndex-1)
-		return root
 	}
-	return build(0, len(inorder)-1)
+
+	return &TreeNode{
+		Val:   rootVal,
+		Left:  buildTree(inorder[:rootIndex], postorder[:rootIndex]),
+		Right: buildTree(inorder[rootIndex+1:], postorder[rootIndex:iLen-1]),
+	}
 }
 
 //
