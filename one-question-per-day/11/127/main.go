7fb3cbbe5b269d33ab1889acb075aa0871252d74
alex.yu <alex.yu@danielwellington.com>
2020-11-05 21:48:54 +0800

    leetcode #127

---
+++ one-question-per-day/11/127/main.go
@@ -0,0 +1,66 @@
+package main
+
+import "math"
+
+func main() {
+
+}
+
+//https://leetcode-cn.com/problems/word-ladder/solution/dan-ci-jie-long-by-leetcode-solution/
+func ladderLength(beginWord string, endWord string, wordList []string) int {
+	wordId := make(map[string]int)
+	var graph [][]int
+	var addWord, addEdge func(string) int
+	addWord = func(word string) int {
+		id, existed := wordId[word]
+		if !existed {
+			id = len(wordId)
+			wordId[word] = id
+			graph = append(graph, []int{})
+		}
+		return id
+	}
+	addEdge = func(word string) int {
+		id1 := addWord(word)
+		s := []byte(word)
+		for i, b := range s {
+			s[i] = '*'
+			id2 := addWord(string(s))
+			graph[id1] = append(graph[id1], id2)
+			graph[id2] = append(graph[id2], id1)
+			s[i] = b
+		}
+		return id1
+	}
+
+	for _, word := range wordList {
+		addEdge(word)
+	}
+	beginId := addEdge(beginWord)
+	endId, has := wordId[endWord]
+	if !has {
+		return 0
+	}
+
+	const inf int = math.MaxInt64
+	dist := make([]int, len(wordId))
+	for i := range dist {
+		dist[i] = inf
+	}
+	dist[beginId] = 0
+	queue := []int{beginId}
+	for len(queue) > 0 {
+		v := queue[0]
+		queue = queue[1:]
+		if v == endId {
+			return dist[endId]/2 + 1
+		}
+		for _, w := range graph[v] {
+			if dist[w] == inf {
+				dist[w] = dist[v] + 1
+				queue = append(queue, w)
+			}
+		}
+	}
+	return 0
+}
