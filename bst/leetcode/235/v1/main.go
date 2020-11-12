0fb4f91b020bd46eca3c44c8fb8c6e85c0f48398
alex.yu <alex.yu@danielwellington.com>
2020-11-12 10:32:44 +0800

    leetcode #922

--- bst/leetcode/235/v1/main.go
+++ bst/leetcode/235/v1/main.go
@@ -7,11 +7,11 @@ func main() {
 /**
 1.若p,q < root，则公共祖先一定在root的左子树中
 2.若p,q > root, 则公共祖先一定在root的右子树中
-
+3.若p,q位于root的两侧或p,q其中之一为root，则其最近公共祖先为root
 */
 //https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-	if root == nil {
+	if root == nil || p == nil || q == nil {
 		return nil
 	}
 	if p.Val < root.Val && q.Val < root.Val {
